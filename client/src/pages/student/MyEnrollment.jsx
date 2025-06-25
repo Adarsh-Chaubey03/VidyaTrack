d0d70ece6b66cf515cb4f4b7ef1b6b4852736767
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-06-25 08:47:12 +0530

    .

--- client/src/pages/student/MyEnrollment.jsx
+++ client/src/pages/student/MyEnrollment.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 function MyEnrollment() {
     return (
-        <div>
-            <h1>Enrollments</h1>
+    <>
+    
+        <div className='md:px-36 px-8 pt-10'>
+            <h1 className='text-2xl font-semibold'>My Enrollments</h1>
+            <table className='md:table-auto table-fixed w-full overflow-hidden border-mt-10'>
+                <thead className=''>
+                    <tr>
+                        <th className='px-4 py-3 font-semibold truncate'>Course </th>
+                        <th className='px-4 py-3 font-semibold truncate'>Duration </th>
+                        <th className='px-4 py-3 font-semibold truncate'>Completed </th>
+                        <th className='px-4 py-3 font-semibold truncate'>Status </th>
+                    </tr>
+                </thead>
+            </table>
         </div>
+
+    </>
     )
 }
 
