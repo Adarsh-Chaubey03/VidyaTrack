82e2242dd791cabb11b293b7d53016e10bfc3c39
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-07-05 17:59:26 +0530

    Update Footer.jsx

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -5,8 +5,8 @@ import { Facebook, Twitter, Linkedin, Mail } from 'lucide-react';
 function Footer() {
   return (
     <footer className="w-full bg-gradient-to-br from-emerald-50 to-emerald-100 border-t border-gray-200 pt-12 pb-4 flex flex-col items-center">
-      <div className="w-full max-w-7xl px-1 md:px-2">
-        <div className="rounded-3xl bg-white shadow-2xl p-4 md:p-6 flex flex-col gap-8">
+      <div className="w-full  px-1 md:px-2">
+        <div className="rounded-3xl mr-15 bg-white shadow-2xl p-6 md:p-6 flex flex-col gap-8">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Brand Section */}
             <div className="flex flex-col items-center text-center">
