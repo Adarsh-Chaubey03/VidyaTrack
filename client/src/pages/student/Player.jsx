23f7badddbff8a53736652b18048e0e60dc8d427
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-07-05 18:42:15 +0530

    Update Player.jsx

--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -223,6 +223,9 @@ function Player() {
                 </div>
               ))}
             </div>
+            <div className='flex items-center gap-2 py-3 mt-10'>
+              <h1 className='text-xl font-bold'>Rate This Course</h1>
+            </div>
           </div>
         </aside>
 
