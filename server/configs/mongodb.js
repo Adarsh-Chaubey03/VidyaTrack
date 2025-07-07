a65b4c1ed1718eaac0dc09ce2d458655068ca966
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-07-07 13:17:15 +0530

    Update mongodb.js

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -2,12 +2,14 @@ import mongoose from "mongoose";
 import { ExportEventTypeOut } from "svix";
 
 // Connect to MongoDB Database
-
 const connectDB = async () => {
-    mongoose.connnection.on('connected', () => 
-        console.log('MongoDB Database Connected'));   
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/vidyatrack`)
+    try {
+        mongoose.connection.on('connected', () => 
+            console.log('MongoDB Database Connected'));
+        await mongoose.connect(process.env.MONGODB_URI); // Remove the extra /vidyatrack here
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+    }
 }
 
-export default connectDB
+export default connectDB;
