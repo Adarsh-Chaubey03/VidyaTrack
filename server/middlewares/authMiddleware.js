575a5367a5187daea2220c810c679f30106c3259
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-07-17 11:01:04 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,9 +1,20 @@
 import { clerkClient } from "@clerk/express";
 
 // Middleware (Protect Educator Routes)
-export const protectEducator = async (req,resizeBy,next)=>{
+export const protectEducator = async (req,res,next)=>{
     try{
-    }catch(error){
+        const userId = req.auth.userId
+        const response = await clerkClient.users.getUser(userId)
+
+         if(response.publicMetadata.role !== 'educator'){
+            return res.json({success: false, message: 'Unauthorized Access'})
+         }
+
+         next()
+
+    } catch(error){
+
+        res.json({succes:false , message: error.message})   
 
     }
 }
